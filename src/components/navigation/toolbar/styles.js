6008a6231d4f4ba946e21dc56287c121b5df3990
fsouza-fs <felipe33737@gmail.com>
2024-07-26 08:36:49 -0300

    Change toolbar color

--- src/components/navigation/toolbar/styles.js
+++ src/components/navigation/toolbar/styles.js
@@ -2,7 +2,8 @@ const styles = {
   header: {
     width: '100%',
     height: '64px',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    // backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    backgroundColor: '#fc0303',
     boxShadow: '3px 3px 8px rgba(0, 0, 0, 0.1)',
     display: 'flex',
     justifyContent: 'space-around',
